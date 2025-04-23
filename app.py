a66f529b7249cc779d1493b7197bc5296db20198
camila2601 <angaritacamila05@gmail.com>
2025-04-23 16:49:13 -0500

    avances 3

--- app.py
+++ app.py
@@ -63,3 +63,16 @@ def descargar_factura(nombre):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
